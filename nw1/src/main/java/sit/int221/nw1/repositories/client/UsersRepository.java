7571cd53b831cc3c6d90d834eebc65fbe7112656
Espesso <110714466+Espesso@users.noreply.github.com>
2024-08-21 14:36:26 +0700

    Delete nw1 directory

--- nw1/src/main/java/sit/int221/nw1/repositories/client/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package sit.int221.nw1.repositories.client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int221.nw1.models.client.Users;
-
-public interface UsersRepository extends JpaRepository<Users, String> {
-    Users findByUsername(String username);
-}
-
